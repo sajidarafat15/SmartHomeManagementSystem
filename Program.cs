fd45b41b053c2c1a72d7fe47f0d535e7e04b59b2
[sajidarafat15] <[sajidarafat15@gmail.com]>
2026-07-05 23:40:55 +0600

    Initial Project Structure

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace SmartHomeManagementSystem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
